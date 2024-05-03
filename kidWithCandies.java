b0deea7981f467659bf4b0bca9bc9a1984b5846d
Colin Kirby <151818456+KirbysGit@users.noreply.github.com>
2024-05-03 15:02:56 -0400

    Update kidWithCandies.java

--- kidWithCandies.java
+++ kidWithCandies.java
@@ -1,7 +1,11 @@
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+// 05 / 03 / 2024 - 3:02 pm
+// sol - find max, add max to all values, check.
+// easy
+// run-time -> 84.98%
+// memory -> 66.24%
 
-
-
-
+// First Attempt...
 class kidWithCandies {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = 0;
