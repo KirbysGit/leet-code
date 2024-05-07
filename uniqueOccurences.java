9de20c5480ee932efd0a40cf35488077024bb9f1
Colin Kirby <151818456+KirbysGit@users.noreply.github.com>
2024-05-06 22:18:52 -0400

    Update uniqueOccurences.java

--- uniqueOccurences.java
+++ uniqueOccurences.java
@@ -33,9 +33,35 @@ class uniqueOccurences {
                 numValues.add(freq.get(item));
             }
         }
+        return true;
+    }
+
+
+    // Second Attempt... 05 / 06 / 2024 - 10:17 pm
+    // Use of .values() in the HashMap API, Didn't know about that.
+    // Gets rid of 2nd HashSet, and saves multiple lines of code.
+    // RT - 96.73%, MEM - 92.94%
+    public boolean uniqueOccurrencesSecond(int[] arr) {
+        
+        HashMap<Integer,Integer> freq = new HashMap<Integer,Integer>();
+        HashSet<Integer> values = new HashSet<Integer>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (freq.containsKey(arr[i])) {
+                freq.put(arr[i], freq.get(arr[i]) + 1);
+            } else {
+                freq.put(arr[i], 1);
+            }
+        }
+
+        for (int item : freq.values()) {
+            if (values.contains(item)) {
+                return false;
+            } else {
+                values.add(item);
+            }
+        }
 
         return true;
-
-        
     }
 }
