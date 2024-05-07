3ba4678dc0fc72ab958a1e00ac9674ee8ea6e475
Colin Kirby <151818456+KirbysGit@users.noreply.github.com>
2024-05-06 22:03:00 -0400

    Create uniqueOccurences.java

---
+++ uniqueOccurences.java
@@ -0,0 +1,37 @@
+
+
+
+class uniqueOccurences {
+
+    // First Attempt... 05 / 06 / 2024 - 10:00 pm
+    // Utilizing HashMap to store frequency of items, and hashset to store individual items.
+    // Then add each frequency of value to another Hashset checking if it is contained per addition, if so, break.
+    // RT - 32.63%, MEM - 30.57%
+    public boolean uniqueOccurrencesFirst(int[] arr) {
+        
+        HashMap<Integer,Integer> freq = new HashMap<Integer,Integer>();
+        HashSet<Integer> values = new HashSet<Integer>();
+        HashSet<Integer> numValues = new HashSet<Integer>();
+
+        for (int i = 0; i < arr.length; i++) {
+            values.add(arr[i]);
+            if (freq.containsKey(arr[i])) {
+                freq.put(arr[i], freq.get(arr[i]) + 1);
+            } else {
+                freq.put(arr[i], 1);
+            }
+        }
+
+        for (int item : values) {
+            if (numValues.contains(freq.get(item))) {
+                return false;
+            } else {
+                numValues.add(freq.get(item));
+            }
+        }
+
+        return true;
+
+        
+    }
+}
