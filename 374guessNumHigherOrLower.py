08dfecb6af69594a54cfeaeeadbaefb78f459b4f
Colin <kirbycolin26@gmail.com>
2025-10-24 15:36:28 -0400

    2300.

--- 374guessNumHigherOrLower.py
+++ 374guessNumHigherOrLower.py
@@ -44,3 +44,27 @@ class firstAttempt:
             attempt = guess(myGuess)
 
         return int(myGuess)
+
+class cleanerAttempt:
+
+    # found it in solutions, simpler, but
+    # i think its same time, maybe slightly better memory.
+
+    def guessNumber(self, n: int) -> int:
+        lo,hi = 1,n
+        while lo <= hi: 
+            mid = (lo+hi)//2
+            if guess(mid) == 0: 
+                return mid
+            elif guess(mid) > 0:
+                lo = mid + 1
+            else: 
+                hi = mid - 1
+
+    # this approach is gas tho, i saw it and was like woah.
+    # but start with a two pointer kind of thing (not really), but
+    # while the values are less than or equal to each other, we keep
+    # going through the middle, then we update the pointers.
+
+    # if guess with mid val is 0, return that. else if 1, 
+    # bring up low. if less than 1, bring down high. 
