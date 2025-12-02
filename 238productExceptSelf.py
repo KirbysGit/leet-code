414f55b7d40404557926f2708f496a63eb06f9e7
Colin <kirbycolin26@gmail.com>
2025-12-02 13:50:50 -0500

    238 but for 150interview.

--- 238productExceptSelf.py
+++ 238productExceptSelf.py
@@ -34,3 +34,26 @@ class firstAttempt:
             right *= nums[i]
 
         return final
+
+class for150problem:
+    
+    # 12 / 02 / 2025 - 1:38 pm
+
+    # set it up like this, really struggled to understand it. 
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        final = [0] * n
+
+        final[0] = 1
+        suffix = 1
+
+        for i in range(1, n):
+            final[i] = nums[i - 1] * final[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix *= nums[i + 1]
+            final[i] = final[i] * suffix
+
+        return final
