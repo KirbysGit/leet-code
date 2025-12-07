220d67599ca8117b01352328ff138c422dd36508
Colin <kirbycolin26@gmail.com>
2025-12-07 13:42:52 -0500

    42.IP

---
+++ 42rainWater.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/trapping-rain-water/
+# hard
+
+class firstAttempt:
+
+    # 12 / 07 / 2025 - 1:24 pm
+
+    # bro i've been working on this for like 2 days, and im still so stuck. 
+    
+    # basically where im at is like i have the general approach of like finding the volumne
+    # left over by the rain. but my issue is handling when i actually check. right now
+    # im carrying two pointers where i only update the back if the front is > than the back.
+
+    # so like i can find the valleys and do the math to find the water left over after displacement.
+
+    # i just asked chatgpt for a hint but i don't know if its helping me that much.
+
+    # i tried implementing a stack for the values and it worked for the two initial test cases but
+    # now my code isn't hadnling the other ones as well. currently at 186 / 324 passed.
