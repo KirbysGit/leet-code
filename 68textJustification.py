48d94aab64187f5a1868b209f95464eb647f33a7
Colin <kirbycolin26@gmail.com>
2025-12-19 15:21:37 -0500

    more notes to 68.

--- 68textJustification.py
+++ 68textJustification.py
@@ -18,6 +18,30 @@ class firstAttempt:
     
     # then the main logic is just iterating through the words, and adding them to the line,
     # and then if the line is too long, we justify it, and then add it to the output.
+    
+    # this is what i wrote out on my whiteboard :
+
+    # iterate through words ::
+    #   checking cur length of line.
+    #   if adding cur word to list w/ a space will be over maxWidth.
+    #       justify row with spaces.
+    #           # do this by working from L -> R, and per space char
+    #           # we run into, check if next char is a space too,
+    #               # if so, continue.
+    #               # else, insert a space and decrement numSpaces.
+    #           # do this until numSpaces is 0.
+    #       then add line to output and reset line to empty.
+    #   else.
+    #       add word with space and continue.
+
+    # some of the extra lings i had to do was handle like a single word case that doesn't fit
+    # entire line, which i handled by checking if " " is not in chars, then we add the spaces 
+    # to end of the line in our addSpaces.
+
+    # also added the leftJustify function to handle that last row case, which now in hindsight
+    # im doing the exact same thing in the addSpaces, but it kind of needed to be separate 
+    # because it would justify the space if there was any in the last row, but the goal is 
+    # just to add the spaces to the end of the row.
 
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
 
