d0f6b7f3c6167cf2234a231501ee86ca6221ef28
Colin <kirbycolin26@gmail.com>
2026-01-09 00:14:01 -0500

    76 much faster.

--- 76minWindowSubstring.py
+++ 76minWindowSubstring.py
@@ -61,3 +61,88 @@ class firstAttempt:
                     del cur[ltr]
             
         return "".join(best)
+
+class gptAssist:
+
+    # 1 / 9 / 2026 - 12:03 am
+
+    # Runtime -> 61 ms - 81.93%
+    # Memory -> 19.99 MB - 5.01%
+    
+    # alright so this takes a pretty similar approach except, instead of 
+    # tracking like the two dictionaries equivalency, its more of two pointer method with
+    # a counter for if the chars have been formed already.
+
+    # starts out with setting up the dictionary for the chars in t, so we know the counts per char.
+    
+    # then we set some variables to help us loop through.
+
+    # looping through we update our cur window with the char at right pointer, then check if
+    # the char count in the cur window is equal to the count in our t dictionary.
+
+    # while our condition for formed is true, we check if window is better than our best window,
+    # if so update it, else we just basically move the left pointer to the right until the condition
+    # that the chars in the cur window are less than the count in our t dictionary.
+
+    # this left pointer moving in allows us to find the smallest window while the condition is still true.
+
+    # code below :
+
+    def minWindow(self, s: str, t: str) -> str:
+
+        # setting up t dictionary for what we need.
+        need = {}
+        for char in t:
+            need[char] = need.get(char, 0) + 1
+
+        # setting up required for our comparator to know if we have all chars we need.
+        required = len(set)
+
+        # setting up some basic variables.
+        left = 0
+        right = 0
+        formed = 0
+        window = {}
+
+        best_len = float('inf')
+        best_start = 0
+
+        # while window is less than len of s.
+        while right < len(s):
+            
+            # grab char at right pointer and add it to our window dict.
+            char = s[right]
+            window[char] = window.get(char, 0) + 1
+
+            # if the char is in our need dict and the count of the char is now equal, increment our formed counter.
+            if char in need and window[char] == need[char]:
+                formed += 1
+
+            # while formed counter is equal to required, check if window is better than our best window.
+            while formed == required:
+                
+                # if so, update our best window variables.
+                if right - left + 1 < best_len:
+                    best_len = right - left + 1
+                    best_start = left
+                
+                # grab char at left pointer and decrement it from our window dict.
+                left_char = s[left]
+                window[left_char] -= 1
+
+                # if char is in need dict and the count of the char is now less than the count in need dict, decrement formed.
+                if left_char in need and window[left_char] < need[left_char]:
+                    formed -= 1
+
+                # increment left pointer to slide window right.
+                left += 1
+            
+            # increment right pointer to slide window right.
+            right += 1
+
+        # if best len is still inf, return empty string, else return the best window.
+        if best_len == float('inf'):
+            return ""
+        else:
+            return s[best_start : best_start + best_len]
+
