5511db95e155d1c99d8b88829778b5ef90af0247
Colin <kirbycolin26@gmail.com>
2026-01-25 20:46:48 -0500

    128 one working solution.

--- 128longestConsecutiveSequence.py
+++ 128longestConsecutiveSequence.py
@@ -33,3 +33,52 @@ class firstAttempt:
                 check[nums[idx]].add(nums[idx])
 
         print(check)
+
+class workingAttempt:
+
+    # 01 / 25 / 2026 - 8:23 pm
+
+    # Runtime -> 47 ms - 69.53%
+    # Memory -> 36.68 MB - 32.53%
+
+    # had to use chatgpt for this, i was really trying to understand how to use the sets, like i was
+    # using a dictionary of sets, but it wasn't really working out because i would get these partial
+    # sets. 
+
+    # i used chatgpt for some hints, it didn't give me the direct answer but it mentioned just using
+    # a set as a seen array and i was like ohhh im stupid asf!! like just using a set to add values
+    # then referencing the set to see if any sequences are possible from the values.
+
+    # but in my mind it still is pretty weird that its not O(n^2) as we use a while loop, but i guess
+    # thats just the power of sets and how they work.
+
+    # so this approach below with the help of chatgpt really just creates a seen set, then goes back
+    # through checking if we've seen the value - 1, we skip because that means we will count it
+    # at some other point so this helps us avoid counting through partial sequences twice.
+
+    # if we haven't seen it then we iterate through the values that exist checking if they are
+    # in the set, and increment our sequence length pointer and update the max if its greater.
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        if not nums:
+            return 0
+        
+        out = set()
+
+        for num in nums:
+            out.add(num)
+
+        best = 0
+
+        for num in nums:
+            if num - 1 not in out:
+                val = num
+                seq = 1
+                while val + 1 in out:
+                    val += 1
+                    seq += 1
+                
+                best = max(best, seq)
+
+        return best
