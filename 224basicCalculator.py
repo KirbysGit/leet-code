242840ef85259e6749f62387546ed4e49037e91a
Colin <kirbycolin26@gmail.com>
2026-02-07 16:07:13 -0500

    224 work in progress.

---
+++ 224basicCalculator.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/basic-calculator/
+# hard
+
+class firstAttempt:
+
+    # 02 / 07 / 2026 - 4:05 pm
+
+    # still working throuhg it right now. 
+    # its def a struggle handling the parenthesis and knowing the order
+    # of operators with that.
+    # just had the lightbulb of PEMDAS, but we'll see if i can really
+    # implement that.
+
+    # right now just trying to figure out how to hanlde like
+    # when to know to do each one. like you need to do substraction
+    # before you add to stack.
+
+    
