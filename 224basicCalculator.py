2c7f1b68e2e39d5d33255af47c6659d9d73592d8
Colin <kirbycolin26@gmail.com>
2026-02-08 23:19:43 -0500

    224 wip.

--- 224basicCalculator.py
+++ 224basicCalculator.py
@@ -15,4 +15,55 @@ class firstAttempt:
     # when to know to do each one. like you need to do substraction
     # before you add to stack.
 
+    def calculate(self, s: str) -> int:
+
+        operators = ["+", "-", "*", "/"]
+
+        s = s.replace(" ", "")
+        s = list(s)
+        s.append(")")
+
+        nums = []
+        ops = []
+
+        print(s)
+        
+        for char in s:
+            if char == "(":
+                continue
+        
+            if len(nums) == 2:
+                print(nums)
+                print(ops)
+                while ops:
+                    x = nums.pop()
+                    y = nums.pop()
+                    op = ops.pop()
+                    nums.append(self.solve(x, y, op))
+
+            if char in operators:
+                ops.append(char)
+            else:
+                nums.append(int(char))
+
+        return nums[0]
     
+
+    def solve(self, x, y, op):
+        if op == '+':
+            return y + x
+        elif op == '-':
+            return y - x
+        elif op == '*':
+            return y * x
+        else:
+            return y / x
+
+
+class secondAttempt:
+
+    # 02 / 08 / 2026 - 11:19 pm
+    
+    # was drinking at superbowl party. wanna finish this shit, but need a git commit rn. 
+    
+    # working on it rn.
