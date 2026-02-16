9a00a274dba7f447cf929635afde80b34bcc57d3
Colin <kirbycolin26@gmail.com>
2026-02-16 16:35:10 -0500

    138 start.

---
+++ 138copyListWithRandomPointer.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/copy-list-with-random-pointer/
+# medium
+
+class firstAttempt:
+
+    # 02 / 16 / 2026 - 4:33 pm
+
+    # bro i've been so fucking exhausted this morning i literally used
+    # chatgpt to solve the problem and i still don't even know how to do it.
+
+    # im coming back to this problem tmrw and i'll solve it on my own, i have
+    # no recollection of what chatgpt told me so it will basically be 
+    # starting out fresh!!
+
+    # still super fucking tired right now so imma just pause.
+
+    # idea was like two pass, one for list and one for random pointers.
+
+    # in my initial attempt i was trying to understand how to use
+    # a hashmap to store the nodes so i could reference them properly.
