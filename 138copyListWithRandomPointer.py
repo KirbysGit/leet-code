ecd2797556f2fc10aafb8f264cac33a18d01f32e
Colin <kirbycolin26@gmail.com>
2026-02-17 21:08:41 -0500

    138. done.

--- 138copyListWithRandomPointer.py
+++ 138copyListWithRandomPointer.py
@@ -18,3 +18,36 @@ class firstAttempt:
 
     # in my initial attempt i was trying to understand how to use
     # a hashmap to store the nodes so i could reference them properly.
+
+class correct:
+
+    # 02 / 17 / 2026 - 9:06 pm
+
+    # Runtime -> 45 ms - 58.58%
+    # Memory -> 20.10 MB - 43.79%
+
+    # i'm still kind of lost with this. linkedlists are so fucking confusing to me.
+
+    # basically its like we copy everything over with a hashmap, but we do it in two passes.
+
+    # im just lost on the connections between.
+    
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+        
+        mapping = {}
+
+        cur = head
+        while cur is not None:
+            mapping[cur] = Node(cur.val)
+            cur = cur.next
+
+        cur = head
+        while cur is not None:
+            mapping[cur].next = mapping.get(cur.next)
+            mapping[cur].random = mapping.get(cur.random)
+
+            cur = cur.next
+
+        return mapping[head]
