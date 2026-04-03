4c8876d03bb4d1e5387248f2689a6c5de4e4220e
Colin <kirbycolin26@gmail.com>
2026-04-03 14:45:18 -0400

    117 done!

--- 117populatingNextRightPointersInEachNodeII.py
+++ 117populatingNextRightPointersInEachNodeII.py
@@ -37,3 +37,57 @@ class tryingToPlan:
             if top.right:
                 queue.append(top.right)
         
+class imTheGoat:
+
+    # 04 / 03 / 2026 - 2:41 pm
+
+    # Runtime -> 44 ms - 91.67%
+    # Memory -> 20.43 MB - 45.55%
+
+    # i was coming back, literally saying to myself like, lets not get married to this approach
+    # trying to just use a singular bfs to track the next node as we cant tell the split.
+    
+    # i tried to figure out patterns, i drew out BFS right and left at same time to see if
+    # we could connect them, but iterations got weird so i didn't go too far into that one.
+
+    # then i drew out the preorder, inorder, and postorder to see if there is a way to connect
+    # those values but it didn't really seem like one.
+
+    # so i came back to the bfs and was planning out how to split the rows based on whats 
+    # picked up per queue run, and i had the hidea to split it into multiple while loops
+    # to handle the different rows. 
+
+    # like two sorts of queues, one for this current level's node, then one for then next level's nodes.
+
+    # so the BFS queue would expand as it moves downwards and connect across in the way the nodes
+    # are seen in a BFS.
+    
+    # i implemneted that, then it worked!!
+
+    # here is the code : 
+
+    def connect(self, root: 'Node') -> 'Node':
+
+        if not root:
+            return root
+
+        toVisit = collections.deque([root])
+        curVisit = collections.deque([])
+
+        while toVisit:
+            curVisit = toVisit
+            toVisit = collections.deque([])
+            while curVisit:
+                top = curVisit.popleft()
+
+                if curVisit:
+                    top.next = curVisit[0]
+                else:
+                    top.next = None
+                
+                if top.left:
+                    toVisit.append(top.left)
+                if top.right:
+                    toVisit.append(top.right)
+        
+        return root
